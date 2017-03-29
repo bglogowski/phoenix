b96fa86a012040f5d18971f908a0b9c1bc69ad25
root <root@puppet.hashrocket.io>
2017-03-28 17:48:26 -0700

    working initial checkin

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 class phoenix (
-    String $hbase_version   = '1.1.9',
-    String $phoenix_version = '4.8.2'
+    String $hbase_version,
+    String $phoenix_version
   ) {
 
-  class { 'hbase::phoenix::install':
+  class { 'phoenix::install':
     hbase_version   => $hbase_version,
     phoenix_version => $phoenix_version,
     require         => Class['::hbase'],
